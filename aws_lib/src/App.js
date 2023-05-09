6f8c535e9019ad2ada2c67efe32a8181e49d7d74
steph45607 <staniswinata.stephanie@gmail.com>
2023-05-09 21:08:33 +0700

    initialized react and bootstrap

---
+++ aws_lib/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <navbar>AWS Library</navbar>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+// import ThemeSwitcher from "./ThemeSwitcher";
+
+// function App() {
+//   return (
+//     <div className="App min-vh-100 d-flex justify-content-center align-items-center">
+//       <div>
+//         <ThemeSwitcher />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
