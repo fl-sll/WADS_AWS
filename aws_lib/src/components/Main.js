537c78f4b58141ccd1804a12f5bee3c38895acd0
steph45607 <staniswinata.stephanie@gmail.com>
2023-06-01 22:07:58 +0700

    added carousel

--- aws_lib/src/components/Main.js
+++ aws_lib/src/components/Main.js
@@ -4,31 +4,19 @@ import bookBtn from "../assets/BookBtn.png"
 import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faNewspaper, faMarker, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
-// import ImageGallery from 'react-image-gallery'
+import Carousel from "./Carousel"
 
-// const images = [
-//     {
-//         original: "../assets/available.png",
-//         thumbnail: "../assets/available.png"
-//     },
-//     {
-//         original: "../assets/backgroundLogin.png",
-//         thumbnail: "../assets/backgroundLogin.png"
-//     }
-// ]
-
-// class MyGallery extends React.Component{
-//     render(){
-//         return <ImageGallery items={images}></ImageGallery>
-//     }
-// }
 
 function Main(){
+    const slides = [
+        {url:"https://riba-prd-assets.azureedge.net/-/media/Riba/Images/Block-Images/Reading-room-promo-image.jpg?rev=1e9d0af0e7dd43308ecb60e34a9b86bc&h=613&w=956&la=en&hash=83F6A6C9CA46CB126B07868125880AA5", title: 'library1'},
+        {url:"https://assets.weforum.org/article/image/JMF96ETfn1kSViVnUou1Z0XIDwWcPpT5mrPc7-ytpAc.jpg", title: 'library2'},
+        {url:"https://i.pinimg.com/736x/03/f1/bd/03f1bd269a5eaceb87e77da976d6c5f9.jpg", title: 'library3'}
+    ]
     return(
         <div className='container'>
             <div className='carousel'>
-                {/* <MyGallery></MyGallery> */}
-                {/* <ImageGallery items={images}></ImageGallery> */}
+                <Carousel slides = {slides}/>
             </div>
             <div className='searchBar'>
                 <p>Search our collections</p>
