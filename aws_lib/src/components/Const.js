d831442aeac7912e46244a23add0a00a5df05cee
steph45607 <staniswinata.stephanie@gmail.com>
2023-06-20 21:58:32 +0700

    backend link

--- aws_lib/src/components/Const.js
+++ aws_lib/src/components/Const.js
@@ -1,2 +1,2 @@
-// export const BACKEND_LINK = "https://aws-wads-o7f3eoioba-an.a.run.app";
-export const BACKEND_LINK = "http://127.0.0.1:8000";
+export const BACKEND_LINK = "https://aws-wads-o7f3eoioba-an.a.run.app";
+// export const BACKEND_LINK = "http://127.0.0.1:8000";
